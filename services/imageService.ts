3687dc18f6537151efa42227306c3966de926dd0
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-01-28 14:14:25 +0530

    add imageService for file upload to Cloudinary and profile image retrieval

---
+++ services/imageService.ts
@@ -0,0 +1,57 @@
+import { CLOUDINARY_NAME, CLOUDINARY_UPLOAD_PRESET } from '@/constants';
+import axios from 'axios';
+
+const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_NAME}/image/upload`;
+
+export const uploadFileToCloudinary = async (file: any, folderName: string) => {
+    try {
+        if (!file || !file.uri) return { success: false, msg: "No file provided" };
+
+        const formData = new FormData();
+        
+        // MimeType eka dynamic ganna eka thama 400 error eka nathi karanna hodama widiha
+        const type = file.mimeType || "image/jpeg";
+        const name = file.fileName || file.uri.split("/").pop() || "upload.jpg";
+
+        formData.append("file", {
+            uri: file.uri,
+            type: type,
+            name: name,
+        } as any);
+
+        formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+        formData.append("folder", folderName);
+
+        const response = await axios.post(CLOUDINARY_API_URL, formData, {
+            headers: { "Content-Type": "multipart/form-data" },
+        });
+
+        return { success: true, data: response.data.secure_url };
+    } catch (error: any) {
+        console.log("Cloudinary Upload Error Details:", error.response?.data || error.message);
+        return { success: false, msg: error.message };
+    }
+};
+
+// export const getProfileImage = (file: any) => {
+//     if (!file) return require("../assets/images/defaultProfile.png");
+//     if (typeof file === 'string' || typeof file === 'object') return { uri: file };
+//     if (file.uri) return { uri: file.uri };
+//     return require("../assets/images/defaultProfile.png");
+// };
+
+export const getProfileImage = (file: any) => {
+    if (!file) {
+        return require("../assets/images/defaultProfile.png");
+    }
+
+    if (typeof file === 'string') {
+        return { uri: file };
+    }
+
+    if (typeof file === 'object' && file.uri) {
+        return { uri: file.uri };
+    }
+
+    return require("../assets/images/defaultProfile.png");
+}
